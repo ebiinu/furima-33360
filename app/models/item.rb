92eda0c6d89e8defd303efb6816ff050f73d9056
ebiinu <supertatsu1318@gmail.com>
2021-03-08 13:06:56 +0900

    アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
-  
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee_payer
+  belongs_to :prefecture
+  belongs_to :preparing_day
 end
