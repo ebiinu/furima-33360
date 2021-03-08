3ec69211d85998b77652f1989223aaec5041c122
ebiinu <supertatsu1318@gmail.com>
2021-03-08 20:25:17 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { 'test' }
+    image                 { 'test_image.png' }
+    introduction          { "test" }
+    category_id           { 2 }
+    condition_id          { 2 }
+    delivery_fee_payer_id { 2 }
+    prefecture_id         { 2 }
+    preparing_day_id      { 2 }
+    price                 { 2222 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+    association :user
   end
 end
