db6c5cde0fee512a253423281051ab79131854c4
ebiinu <supertatsu1318@gmail.com>
2021-03-10 11:56:09 +0900

    出品者以外、未ログイン者の強制遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -22,14 +23,26 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    
+    @item = Item.find(params[:id])
   end
 
   def update
-    
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else  
+      render :edit
+    end
   end
   private
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :category_id, :condition_id, :price, :delivery_fee_payer_id, :prefecture_id, :preparing_day_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
