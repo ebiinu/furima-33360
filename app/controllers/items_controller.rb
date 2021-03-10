1be3546b76ec2709262ff9d0cac7b71b087f22ed
ebiinu <supertatsu1318@gmail.com>
2021-03-10 12:48:21 +0900

    修正1回目
    @item = Item.find(params[:id])
    という同じ記述をまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:edit, :show, :update]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -19,21 +20,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
       redirect_to item_path
     else  
       render :edit
     end
   end
+  
   private
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :category_id, :condition_id, :price, :delivery_fee_payer_id, :prefecture_id, :preparing_day_id).merge(user_id: current_user.id)
@@ -44,5 +46,9 @@ class ItemsController < ApplicationController
     unless current_user.id == @item.user_id
       redirect_to action: :index
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
