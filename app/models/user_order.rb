f089e321fb9bd3b556a803608014e449072c6c40
ebiinu <supertatsu1318@gmail.com>
2021-03-11 12:26:10 +0900

    Formオブジェクトの作成

---
+++ app/models/user_order.rb
@@ -0,0 +1,19 @@
+class UserOrder
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :city, :house_number,
+  end
+  # 「住所」の都道府県に関するバリデーション
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :phone_number, format: { with: /^\d{10}$|^\d{11}$/, message: "is invalid."}
+
+  def save
+    user = User.create
+    item = Item.create
+    order = Order.create(user_id: user.id, item_id: item.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, order_id: order.id)
+  end
+end
