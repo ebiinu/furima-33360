c25b0eb854021a871e27d02f2feffb5292f98064
ebiinu <supertatsu1318@gmail.com>
2021-03-16 15:18:16 +0900

    コントローラーの修正、商品情報の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,22 @@
 class OrdersController < ApplicationController
   def index
-    @order = UserOrder.new
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
+  def create
+    
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  # 全てのストロングパラメーターを1つに統合
+ def order_params
+  params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+ end
 end
