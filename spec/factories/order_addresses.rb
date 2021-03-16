75480e06f383c330bd14073a8374612afb777c61
ebiinu <supertatsu1318@gmail.com>
2021-03-16 16:19:39 +0900

    order_addressの単体テストコード

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code                  { '206-0021' }
+    prefecture_id                { 2 }
+    city                         { '多摩市' }
+    house_number                 { '連光寺1-29-15' }
+    building_name                { '' }
+    phone_number                 { '09017057739' }
+  end
+end
